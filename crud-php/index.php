1da752a757ed66d1c4031e1b559edb25c6b87eb6
astratow <davno_pora@mail.com>
2022-03-05 06:32:14 +0000

    basic index.html

---
+++ crud-php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP CRUD </title>
+  </head>
+  <body>
+    <form action="" method="POST">
+      <input type="type" name="name" value="value"/>
+      <input type="type" name="name" value="value"/>
+    </form>
+  </body>
+</html>
